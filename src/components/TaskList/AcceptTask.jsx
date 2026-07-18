43e78e8d5e3e9a3065401fc11f718b3a593a6f0a
pankaj <pankajkumarsah940@gmail.com>
2026-07-18 08:34:01 +0530

    passed data in tasks

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const AcceptTask = () => {
+const AcceptTask = ({data}) => {
   return (
-    
         <div className='h-full w-75 p-5 shrink-0 bg-red-400 rounded-xl'>
             <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>28 june 2026</h4>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.taskDate}</h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a you tube video</h2>
-            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit incidunt culpa harum, adipisci consectetur ullam!</p>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+            <p className='text-sm mt-2'>{data.taskDescription}</p>
             <div className='flex justify-between mt-4'>
                 <button className='bg-green-500 py-1 px-2 text-sm'>Mark as Completed</button>
                 <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button>
--- src/others/AllTask.jsx
+++ src/others/AllTask.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthProvider'
 
 const AllTask = () => {
+    const authData = useContext(AuthContext)
   return (
-    <div className='bg-[#1C1C1C] p-5 mt-5 h-48 overflow-auto rounded'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded '>
-            <h2>Pankaj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
+    <div className='bg-[#1C1C1C] p-5 mt-5 rounded'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded ' >
+            <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
+            <h3 className='text-lg font-medium w-1/5'>New Task</h3>
+            <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
+            <h5 className='text-lg font-medium w-1/5'>Completed</h5>
+            <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
-         <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded '>
-            <h2>Pankaj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
+        <div className=' overflow-auto'>
+            {authData.employees.map(function(elem){
+            return <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded ' >
+            <h2 className='text-lg font-medium w-1/5'>{elem.firstName}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskNumber.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-600'>{elem.taskNumber.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-green-600'>{elem.taskNumber.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskNumber.failed}</h5>
         </div>
-         <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded '>
-            <h2>Pankaj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
-        </div>
-         <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded '>
-            <h2>Pankaj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
-        </div>
-         <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded '>
-            <h2>Pankaj</h2>
-            <h3>Make UI Design</h3>
-            <h5>Status</h5>
+        })}
         </div>
     </div>
   )
